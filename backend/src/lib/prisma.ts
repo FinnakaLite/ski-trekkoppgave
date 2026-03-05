b197c56c55f6827ec96b7b6ecc31d39afcdff2e5
FinnakaLite <Finng2008@outlook.com>
2026-03-05 13:09:40 +0100

    Fix Prismas stupid connector

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({ connectionString: `PUT URL HERE TOO` });
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
