55ea7d25e26c8927bcdf5611be234388f542a24f
FinnakaLite <Finng2008@outlook.com>
2026-03-05 13:10:00 +0100

    New backend API routes fixed.

--- backend/src/controllers/miscApi.ts
+++ backend/src/controllers/miscApi.ts
@@ -1,24 +1,29 @@
-import { Request, Response } from 'express';
+import express from 'express';
+import type { Request, Response } from 'express';
 import prisma from '../lib/prisma.ts';
 
 export const verifyCardSerial = async (req: Request, res: Response) => {
-    try {
-         const {cardSerial} = req.query;
-        
-         if (!cardSerial) {
-            return res.status(400)
-         }
+    console.log(req.query)
+   try {
+      const { cardSerial } = req.query;
 
-         const entry = await prisma.heis_Turer.findFirst({
-            where: {
-                cardSerial: String(cardSerial)
-            }
-         })
+      if (!cardSerial) {
+         return res.status(400)
+      }
 
-         if (entry) {
-            return res.status(200)
-         } else {
-            return res.status(404)
+      const entry = await prisma.heis_Turer.findFirst({
+         where: {
+            cardSerial: String(cardSerial)
          }
-    }
-}
+      })
+
+      if (entry) {
+         return res.status(200).json({ message: 'Card serial is valid' })
+      } else {
+         return res.status(404).json({ error: 'Card serial not found' })
+      }
+   } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Internal server error' });
+   }
+};
